5fbabdb6270370aafa6a29eb0310fce2d0e2f4db
Satinder Singh Sall. <130855316+SatinderSinghSall@users.noreply.github.com>
2025-10-12 22:24:25 +0530

    Added: OnCollisionEnter2D() in the Game.

---
+++ Assets/Collision.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Ouch! - Collision Occured.");
+    }
+}
